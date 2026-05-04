647399f98106fba8dc37829dc5054ea065cc4cbe
Offichigo <oph.perianin@gmail.com>
2026-05-04 15:55:51 +0200

    table themes crée

--- migration_up.sql
+++ migration_up.sql
@@ -1,2 +1,10 @@
-CREATE TYPE resource_type AS ENUM ('guide', 'video', 'exercice', 'projet');
-CREATE IF NOT EXISTS
+-- CREATE TYPE IF NOT EXISTS resource_type AS ENUM ('guide', 'video', 'exercice', 'projet');
+CREATE TABLE IF NOT EXISTS themes
+(
+    id integer ,
+    name text,
+    description text,
+    created_at timestamptz,
+    updated_at timestamptz,
+    PRIMARY KEY (id)
+);
